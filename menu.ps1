b002bfc5d62115ac80ec0b4721b688c0f5687d66
wcloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-03-03 10:15:53 +1030

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -1,4 +1,5 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/menu_items.csv" -OutFile "C:\Windows\Temp\menu_items.csv"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSScriptMenuGui
 Set-Location $PSScriptRoot
 Import-Module PSScriptMenuGui -ErrorAction Stop
