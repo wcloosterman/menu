c0c74c7d1af70ab916801a639931a58108acbade
wcloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-03-03 10:13:19 +1030

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -1,16 +1,5 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/menu_items.csv" -OutFile "C:\Windows\Temp\menu_items.csv"
 Install-Module -Name PSScriptMenuGui
-#region Setup
 Set-Location $PSScriptRoot
-Remove-Module PSScriptMenuGui -ErrorAction SilentlyContinue
-try {
-    Import-Module PSScriptMenuGui -ErrorAction Stop
-}
-catch {
-    Write-Warning $_
-    Write-Verbose 'Attempting to import from parent directory...' -Verbose
-    Import-Module '..\'
-}
-#endregion
-
+Import-Module PSScriptMenuGui -ErrorAction Stop
 Show-ScriptMenuGui -csvPath 'C:\Windows\Temp\menu_items.csv' -Verbose
