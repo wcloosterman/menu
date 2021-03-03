3b06e7fb7f49c03ccc7c50cf423f2870f346e75b
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-03-04 10:29:34 +1030

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -2,7 +2,7 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     throw "You didn't run this script as an Administrator. Please start Powershell as an administrator and run again."
 }
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/menu_items.csv" -OutFile "C:\Windows\Temp\menu_items.csv"
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSScriptMenuGui
 Set-Location $PSScriptRoot
 Import-Module PSScriptMenuGui -ErrorAction Stop
