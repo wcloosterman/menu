78c7c4e468ba4e5682633e190c4ec5cfc1562ce5
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-04-01 09:22:12 +1030

    Update Add Rescue Logo.ps1

--- Scripts/Add Rescue Logo.ps1
+++ Scripts/Add Rescue Logo.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "SilentlyContinue"
 New-Item -ItemType Directory -Force -Path C:\Support
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/wcloosterman/menu/blob/main/files/RESCUE.zip","C:\Support\RESCUE.zip")
+$WebClient.DownloadFile("https://github.com/wcloosterman/menu/raw/main/files/RESCUE.zip","C:\Support\RESCUE.zip")
 Expand-Archive "C:\Support\RESCUE.zip" -DestinationPath "C:\Support"
 Move-Item -Path "C:\Support\Commuserv RESCUE.url" -Destination "C:\Users\Public\Desktop\Commuserv RESCUE.url" -Force
 Remove-Item -Path "C:\Support\RESCUE.zip"
