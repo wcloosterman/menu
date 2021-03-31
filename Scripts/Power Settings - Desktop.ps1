5800419028910e97887ed7e45afaa9d660d513fe
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-03-31 17:14:16 +1030

    Update Power Settings - Desktop.ps1

--- Scripts/Power Settings - Desktop.ps1
+++ Scripts/Power Settings - Desktop.ps1
@@ -6,3 +6,6 @@ Powercfg /Change standby-timeout-ac 300
 
 # Disable USB Selective Suspend
 powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+
+# Make powerscheme active immediately
+powercfg /setactive scheme_current
