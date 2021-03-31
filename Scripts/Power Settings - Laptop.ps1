1e7095eec346a08c0a64403067ac67ed216709ea
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-03-31 16:57:55 +1030

    Update Power Settings - Laptop.ps1

--- Scripts/Power Settings - Laptop.ps1
+++ Scripts/Power Settings - Laptop.ps1
@@ -1,11 +1,17 @@
-# Monitor Settings - 20 Minutes
+# Monitor Settings
 Powercfg /Change monitor-timeout-dc 15
+Powercfg /Change monitor-timeout-ac 60
 
-# Sleep Settings - 5 Hours
+# Sleep Settings
 Powercfg /Change standby-timeout-dc 60
+Powercfg /Change standby-timeout-ac 300
 
 # Disable USB Selective Suspend
 powercfg /SETDCVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
 
-#Do Nothing When Closing Lid
+# Do Nothing When Closing Lid
+powercfg /setacvalueindex scheme_current sub_buttons lidaction 0
 powercfg /setdcvalueindex scheme_current sub_buttons lidaction 0
+
+
