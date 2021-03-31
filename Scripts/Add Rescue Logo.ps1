fefc2e244448d07df2e6c5164f5c468e65615251
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-04-01 09:06:30 +1030

    Update Add Rescue Logo.ps1

--- Scripts/Add Rescue Logo.ps1
+++ Scripts/Add Rescue Logo.ps1
@@ -1,5 +1,7 @@
 New-Item -ItemType Directory -Force -Path C:\Support
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/wcloosterman/menu/main/files/favicon.ico","C:\Support\csv_icon.ico")
-Expand-Archive C:\Support\CSV_ICON.ZIP C:\Support -Force
+$WebClient.DownloadFile("https://raw.githubusercontent.com/wcloosterman/menu/main/files/Commuserv%20RESCUE.url","C:\Support\Commuserv RESCUE.url")
 Move-Item -Path "C:\Support\Commuserv RESCUE.url" -Destination "C:\Users\Public\Desktop\Commuserv RESCUE.url" -Force
+
+
