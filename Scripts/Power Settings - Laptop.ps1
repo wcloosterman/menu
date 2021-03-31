508d8273c5f73bdf741bdd722e25369a7059398b
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-03-31 16:48:28 +1030

    Create Power Settings - Laptop.ps1

---
+++ Scripts/Power Settings - Laptop.ps1
@@ -0,0 +1,8 @@
+# Monitor Settings - 20 Minutes
+Powercfg /Change monitor-timeout-ac 20
+
+# Sleep Settings - 5 Hours
+Powercfg /Change standby-timeout-ac 300
+
+# Disable USB Selective Suspend
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
