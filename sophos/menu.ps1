081be20cbbdab9d980acbcca03307889a10e3bae
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-05-05 16:42:06 +0930

    Create menu.ps1

---
+++ sophos/menu.ps1
@@ -0,0 +1,9 @@
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    throw "You didn't run this script as an Administrator. Please start Powershell as an administrator and run again."
+}
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/menu_items.csv" -OutFile "C:\Windows\Temp\menu_items.csv"
+#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PSScriptMenuGui
+#Set-Location $PSScriptRoot
+Import-Module PSScriptMenuGui -ErrorAction Stop
+Show-ScriptMenuGui -csvPath 'C:\Windows\Temp\menu_items.csv' -hideConsole
