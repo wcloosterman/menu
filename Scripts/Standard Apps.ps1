c42c696dcaff3665a42c60d6667969d8bf7e6fa8
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-05-11 09:43:29 +0930

    Update Standard Apps.ps1

--- Scripts/Standard Apps.ps1
+++ Scripts/Standard Apps.ps1
@@ -1,6 +1,6 @@
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 $packages = @{
-  "firefox" = "1"
+  "firefox" = "0"
   "google-chrome-for-enterprise" = "1"
   "7zip" = "1"
   "adobereader" = "1"
