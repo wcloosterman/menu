9acae62c40e6f181639e0cad23337b6b2ac5aa6a
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-05-11 10:10:29 +0930

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -1,13 +1,14 @@
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
     throw "You didn't run this script as an Administrator. Please start Powershell as an administrator and run again."
 }
+mkdir C:\Support -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/menu_items.csv" -OutFile "C:\Windows\Temp\menu_items.csv"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module -Name PSScriptMenuGui
 Set-Location $PSScriptRoot
 Import-Module PSScriptMenuGui -ErrorAction Stop
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/files/favicon.ico" -OutFile "C:\Windows\Temp\favicon.ico"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/files/favicon.ico" -OutFile "C:\Support\favicon.ico"
 $params = @{
     csvPath = 'C:\Windows\Temp\menu_items.csv'
     windowTitle = 'Commuserv Shared Scripts'
@@ -16,6 +17,6 @@ $params = @{
     hideConsole = $true
     noExit = $true
     Verbose = $true
-    iconpath = 'C:\Windows\Temp\favicon.ico'
+    iconpath = '..\..\..\Support\favicon.ico'
 }
 Show-ScriptMenuGui @params
