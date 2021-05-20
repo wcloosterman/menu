46d4754e088d64f4960b5b47e9dd47cc2d03e116
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-05-20 17:15:33 +0930

    Update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -3,8 +3,8 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 }
 mkdir C:\Support -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/wcloosterman/menu/main/menu_items.csv" -OutFile "C:\Windows\Temp\menu_items.csv"
-#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSScriptMenuGui
 Set-Location $PSScriptRoot
 Import-Module PSScriptMenuGui -ErrorAction Stop
