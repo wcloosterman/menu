e556936b3ad3dce7aea7263514ad8666b67ac262
Wade Cloosterman <77086938+wcloosterman@users.noreply.github.com>
2021-05-20 17:22:40 +0930

    Delete Rename Machine.ps1

--- Scripts/Rename Machine.ps1
+++
@@ -1,7 +0,0 @@
-$hostname = [System.Net.Dns]::GetHostName()
-$input = $(
-      Add-Type -AssemblyName Microsoft.VisualBasic
-      [Microsoft.VisualBasic.Interaction]::InputBox('New machine name','RENAME MACHINE', $hostname)
-     )
-
-Rename-Computer -Newname $input -Force
